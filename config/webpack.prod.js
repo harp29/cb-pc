2a95cf207d6e44dd0640ba5b3ab1580dbd5168b2
Harpreet Singh <info@designsbyharp.com>
2018-07-06 08:11:10 -0400

    webpack.prod.js docs folder for output and ran build task

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -19,7 +19,7 @@ module.exports = env => {
     mode: "production",
     output: {
       filename: "[name]-bundle.js",
-      path: path.resolve(__dirname, "../dist"),
+      path: path.resolve(__dirname, "../docs"),
       publicPath: "/"
     },
     module: {
